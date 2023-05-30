20e6e1e44aaa04b08d945a8136aa5c8d1410c99f
richod@1 <frimpsup@gmail.com>
2023-05-29 19:25:31 -1200

    user-auth

---
+++ backend/controller/authController/adminAuth.js
@@ -0,0 +1,12 @@
+const jwt=require('jsonwebtoken')
+const {createJWT}=require('../../utils/auth.js')
+const Admin=require('../../models/admin/admin.model.js')
+const bcrypt=require('bcrypt')
+require('dotenv').config()
+
+
+
+
+
+
+
