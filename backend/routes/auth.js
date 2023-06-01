b7948a43a4968996f645dff4a73d6e792d74ab11
richod@1 <frimpsup@gmail.com>
2023-06-01 09:32:32 -1200

    router-updated

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,12 +1,21 @@
 const express=require('express')
 const router=express.Router()
 
-const {signUp,signIn}=require('../controller/authController/auth.js')
+const {signUp,signIn,updateUser,
+userById,resetPassword,deleteUser}=require('../controller/authController/auth.js')
 
 
 router.post('/signUp',signUp)
 
 
 router.post('/signIn',signIn)
+
+router.post('/update-user',updateUser)
+
+router.get('/user:id',userById)
+
+router.post('/reset-pass',resetPassword)
+
+router.get('/delete-user',deleteUser)
 
 module.exports=router;
